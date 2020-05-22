fd0379ff257843582036a9298013d67af9c5e485
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-22 15:53:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,13 @@
 class Shoe
   attr_reader :brand
-  
+
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
 
